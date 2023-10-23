0e90e5f8d60ccda7462359ff2bc335a6904503b2
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2023-10-23 16:54:55 +0530

    fix:ds

--- src/app/views/home/slider.tsx
+++ src/app/views/home/slider.tsx
@@ -6,9 +6,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 export const Slider = () => {
   return (
@@ -18,23 +19,26 @@ export const Slider = () => {
         pagination={{
           dynamicBullets: true,
         }}
-        modules={[Pagination]}
+        navigation={true}
+        modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <Image src="/carousel/3.jpg" width={1000} height={400} alt="sda" />
+          <img src="/carousel/Slider-01.png" alt="sda" />
+          <div className="z-50 absolute">
+            <div className="text text--enter" data-scroll="out" data-splitting="">
+              Enter
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/carousel/2.jpg" width={1000} height={400} alt="sda" />
+          <img src="/carousel/Slider-02.png" alt="sda" />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/carousel/3.jpg" width={1000} height={400} alt="sda" />
+          <img src="/carousel/Slider-03.png" alt="sda" />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/carousel/4.jpg" width={1000} height={400} alt="sda" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/carousel/5.jpg" width={1000} height={400} alt="sda" />
+          <img src="/carousel/Slider-04.png" alt="sda" />
         </SwiperSlide>
       </Swiper>
     </div>
