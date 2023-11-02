422798e916494349399a143cb3e619805c01b80c
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2023-11-02 18:42:21 +0530

    fix:footer

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -1,34 +1,22 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
   const navigation = {
     solutions: [
-      { name: "Marketing", href: "#" },
-      { name: "Analytics", href: "#" },
-      { name: "Commerce", href: "#" },
-      { name: "Insights", href: "#" },
+      { name: "Oil & Gas Projects", href: "#" },
+      { name: "Renewable Power Projects", href: "#" },
+      { name: "Agriculture Projects", href: "#" },
+      { name: "Healthcare", href: "#" },
+      { name: "Trade and Finance", href: "#" },
+      { name: "Commodities", href: "#" },
+      { name: "Corporate Training and Development", href: "#" },
+      { name: "Capital Solicitation and Finance", href: "#" },
     ],
-    support: [
-      { name: "Pricing", href: "#" },
-      { name: "Documentation", href: "#" },
-      { name: "Guides", href: "#" },
-      { name: "API Status", href: "#" },
-    ],
-    company: [
-      { name: "About", href: "#" },
-      { name: "Blog", href: "#" },
-      { name: "Jobs", href: "#" },
-      { name: "Press", href: "#" },
-      { name: "Partners", href: "#" },
-    ],
-    legal: [
-      { name: "Claim", href: "#" },
-      { name: "Privacy", href: "#" },
-      { name: "Terms", href: "#" },
-    ],
+
     social: [
       {
         name: "Facebook",
@@ -65,19 +53,7 @@ export const Footer = () => {
           </svg>
         ),
       },
-      {
-        name: "GitHub",
-        href: "#",
-        icon: (props: any) => (
-          <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-            <path
-              fillRule="evenodd"
-              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-              clipRule="evenodd"
-            />
-          </svg>
-        ),
-      },
+
       {
         name: "YouTube",
         href: "#",
@@ -100,11 +76,14 @@ export const Footer = () => {
         Footer
       </h2>
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-16">
           <div className="space-y-8">
             <Image src="/footer-logo.svg" width={190} height={70} alt="HyperCapital" />
-            <p className="text-sm leading-6 text-gray-300">
-              Making the world a better place through constructing elegant hierarchies.
+            <p className="text-sm leading-6 font-light text-gray-300">
+              HyperCapital Inc. is a global trade, finance and investment consulting firm that collaborates
+              and partners with private and public sector companies to help and manage their business
+              finances, commodity procurement challenges and design tactical strategies to leverage and
+              capture their greatest opportunities to meet their financial and invest goals.
             </p>
             <div className="flex space-x-6">
               {navigation.social.map((item) => (
@@ -115,57 +94,47 @@ export const Footer = () => {
               ))}
             </div>
           </div>
-          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm font-semibold leading-6 text-white">Solutions</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.solutions.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="mt-10 md:mt-0">
-                <h3 className="text-sm font-semibold leading-6 text-white">Support</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.support.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+
+          <div>
+            <h3 className="text-base font-semibold leading-6 text-white">OUR EXPERTISE</h3>
+            <ul role="list" className="mt-6 space-y-4">
+              {navigation.solutions.map((item) => (
+                <li key={item.name}>
+                  <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="md:gap-8 space-y-10">
+            <div className="">
+              <h3 className="text-base font-semibold leading-6 text-white">EMAIL ADDRESS</h3>
+              <Link
+                href="mail:info@hypercapitalinc.com"
+                className="mt-6 space-y-4 block text-sm leading-6 text-gray-300 hover:text-white"
+              >
+                info@hypercapitalinc.com
+              </Link>
             </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm font-semibold leading-6 text-white">Company</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.company.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="mt-10 md:mt-0">
-                <h3 className="text-sm font-semibold leading-6 text-white">Legal</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.legal.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
+
+            <div className="">
+              <h3 className="text-base font-semibold leading-6 text-white">EMAIL ADDRESS</h3>
+              <div className="mt-6 space-y-3">
+                {" "}
+                <Link
+                  href="mail:info@hypercapitalinc.com"
+                  className="space-y-4 block text-sm leading-6 text-gray-300 hover:text-white"
+                >
+                  012 456 789 0123
+                </Link>
+                <Link
+                  href="mail:info@hypercapitalinc.com"
+                  className="space-y-4 block text-sm leading-6 text-gray-300 hover:text-white"
+                >
+                  234 567 890 1234
+                </Link>
               </div>
             </div>
           </div>
