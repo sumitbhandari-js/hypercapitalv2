8be0cd0f61fa6449d9c91938f5fb2f0ac3f44246
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2023-11-24 11:47:38 +0530

    fix:image quality

--- src/views/home/slider.tsx
+++ src/views/home/slider.tsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/autoplay";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
@@ -22,10 +23,13 @@ export const Slider = () => {
         }}
         navigation={true}
         modules={[Pagination, Navigation]}
+        effect="fade"
         className="mySwiper"
+        autoplay
       >
+        {/* First Slider  */}
         <SwiperSlide className="">
-          <img src="/carousel/Slider-01.png" alt="sda" />
+          <img src="/carousel/Slider-01.jpg" alt="sda" className="w-full" />
           <div className="absolute h-full hidden py-5 text-left text-white md:block left-[17.5%] w-2/6 top-[18%] bottom-[unset] space-y-10">
             <div className="space-y-3 animate-fade-up">
               <h5 className="text-8xl uppercase font-bold">Building Your Vision</h5>
@@ -41,15 +45,63 @@ export const Slider = () => {
             </div>
           </div>
         </SwiperSlide>
-        {/* <SwiperSlide>
-          <img src="/carousel/Slider-02.png" alt="sda" />
-        </SwiperSlide>
+
+        {/* Second Slider  */}
         <SwiperSlide>
-          <img src="/carousel/Slider-03.png" alt="sda" />
+          <img src="/carousel/Slider-02.jpg" alt="sda" className="w-full" />
+          <div className="absolute h-full hidden py-5 text-left text-white md:block left-[17.5%] w-2/6 top-[18%] bottom-[unset] space-y-10">
+            <div className="space-y-3 animate-fade-up">
+              <h5 className="text-8xl uppercase font-bold">Building Your Vision</h5>
+              <p className="text-2xl font-light">
+                With us you don’t need to guess our work will be sure to impress We’re the construction kings.
+              </p>
+            </div>
+
+            <div className="">
+              <Link href="" className="bg-brand text-sm font-medium px-6 py-4 rounded-sm">
+                CONTACT US
+              </Link>
+            </div>
+          </div>
         </SwiperSlide>
+
+        {/* Third Slider  */}
         <SwiperSlide>
-          <img src="/carousel/Slider-04.png" alt="sda" />
-        </SwiperSlide> */}
+          <img src="/carousel/Slider-03.jpg" alt="sda" className="w-full" />
+          <div className="absolute h-full hidden py-5 text-left text-white md:block left-[17.5%] w-2/6 top-[18%] bottom-[unset] space-y-10">
+            <div className="space-y-3 animate-fade-up">
+              <h5 className="text-8xl uppercase font-bold">Building Your Vision</h5>
+              <p className="text-2xl font-light">
+                With us you don’t need to guess our work will be sure to impress We’re the construction kings.
+              </p>
+            </div>
+
+            <div className="">
+              <Link href="" className="bg-brand text-sm font-medium px-6 py-4 rounded-sm">
+                CONTACT US
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        {/* Fourth Slider  */}
+        <SwiperSlide>
+          <img src="/carousel/Slider-04.jpg" alt="sda" className="w-full" />
+          <div className="absolute h-full hidden py-5 text-left text-white md:block left-[17.5%] w-2/6 top-[18%] bottom-[unset] space-y-10">
+            <div className="space-y-3 animate-fade-up">
+              <h5 className="text-8xl uppercase font-bold">Building Your Vision</h5>
+              <p className="text-2xl font-light">
+                With us you don’t need to guess our work will be sure to impress We’re the construction kings.
+              </p>
+            </div>
+
+            <div className="">
+              <Link href="" className="bg-brand text-sm font-medium px-6 py-4 rounded-sm">
+                CONTACT US
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
